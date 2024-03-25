45cef2ff68498d0ccda607cac49ba385eaab6d1d
EgyipTomi69420 <kovacs.tamas@diak.pataky.hu>
2024-03-25 20:53:07 +0100

    建立網站

---
+++ adam.js
@@ -0,0 +1,41 @@
+//🍝Spaghetti🍝
+
+function madarsko() {
+  if (document.getElementById("cim").innerHTML != "Új-Zéland") {
+    document.getElementById("cim").innerHTML = "Új-Zéland";
+    document.getElementById("cim2").innerHTML = "Utazási terv";
+    document.getElementById("cim3").innerHTML = "Utazás";
+    document.getElementById("cim4").innerHTML = "Szállás";
+    document.getElementById("text1").innerHTML = "1990. 06. 30. 12:00<br>-><br>2046. 04. 14. 9:38<br><br>Sülysáp, repülőtér<br>-><br>Wellington<br><br>Utazási eszköz: repülő";
+    document.getElementById("text2").innerHTML = "Szállás weboldala >";
+    document.getElementById("text3").innerHTML = "Első nap";
+    document.getElementById("text6").innerHTML = "Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu település meglátogatása, a világ leghosszab nevével rendelekző hely.";
+    document.getElementById("text5").innerHTML = "Szállás elfoglalása, városnézés";
+    document.getElementById("text4").innerHTML = "Második nap";
+    document.getElementById("text9").innerHTML = "Utolsó nap";
+    document.getElementById("text7").innerHTML = "Harmadik nap";
+    document.getElementById("text8").innerHTML = "Negyedik nap";
+    document.getElementById("text10").innerHTML = "Sarki fény megtekintése";
+    document.getElementById("text11").innerHTML = "Auckland városának felfedezése";
+    document.getElementById("text12").innerHTML = "Otago biciklis útvonal végigtekerése 1 nap alatt.";
+    //document.getElementById("").innerHTML = "";
+  } else {
+    document.getElementById("cim").innerHTML = "Новая Зеландия";
+    document.getElementById("cim2").innerHTML = "План путешествия";
+    document.getElementById("cim3").innerHTML = "Данные о поездке";
+    document.getElementById("cim4").innerHTML = "Размещение";
+    document.getElementById("text1").innerHTML = "1990. 06. 30. 12:00<br>-><br>2046. 04. 14. 9:38<br><br>Sülysáp, аэропорт <br>-><br>Wellington<br><br>инструмент: самолет";
+    document.getElementById("text2").innerHTML = "Просмотр отеля >";
+    document.getElementById("text3").innerHTML = "Первый день";
+    document.getElementById("text6").innerHTML = "Посещение города Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu, места с самым длинным названием в мире.";
+    document.getElementById("text5").innerHTML = "Бронирование проживания, экскурсии";
+    document.getElementById("text4").innerHTML = "Второй день";
+    document.getElementById("text9").innerHTML = "Последний день";
+    document.getElementById("text7").innerHTML = "Третий день";
+    document.getElementById("text8").innerHTML = "Четвертый день";
+    document.getElementById("text10").innerHTML = "Наблюдение за полярным сиянием";
+    document.getElementById("text11").innerHTML = "Знакомство с городом Окленд";
+    document.getElementById("text12").innerHTML = "Пройдите велосипедный маршрут Отаго за 1 день.";
+    //document.getElementById("").innerHTML = "";
+  }
+}
