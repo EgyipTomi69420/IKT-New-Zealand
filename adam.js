e1899264651ec4d83db7754c42edb35d714f92fc
Ádám <balint.adam@diak.pataky.hu>
2024-04-08 00:58:12 +0200

    changes

--- adam.js
+++ adam.js
@@ -9,7 +9,7 @@ function madarsko() {
     document.getElementById("text1").innerHTML = "2024. 04. 08. 12:00<br>-><br>2024. 04. 09. 19:00<br><br>Budapest, repülőtér<br>-><br>Wellington<br><br>Utazási eszköz: repülő<br>30< óra utazás";
     document.getElementById("text2").innerHTML = "Szállás weboldala >";
     document.getElementById("text3").innerHTML = "Első nap";
-    document.getElementById("text6").innerHTML = "Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu település meglátogatása, a világ leghosszab nevével rendelkező hely.";
+    document.getElementById("text6").innerHTML = "<b>Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu</b> település meglátogatása, a világ leghosszab nevével rendelkező hely.";
     document.getElementById("text5").innerHTML = "Szállás elfoglalása, városnézés";
     document.getElementById("text4").innerHTML = "Második nap";
     document.getElementById("text9").innerHTML = "Utolsó nap";
@@ -17,7 +17,7 @@ function madarsko() {
     document.getElementById("text8").innerHTML = "Negyedik nap";
     document.getElementById("text10").innerHTML = "Sarki fény megtekintése";
     document.getElementById("text11").innerHTML = "Auckland városának felfedezése";
-    document.getElementById("text12").innerHTML = "Otago biciklis útvonal végigtekerése 1 nap alatt.";
+    document.getElementById("text12").innerHTML = "Otago biciklis útvonal végigtekerése 1 nap alatt";
   } else {
     document.getElementById("cim").innerHTML = "Новая Зеландия";
     document.getElementById("cim2").innerHTML = "План путешествия";
@@ -26,7 +26,7 @@ function madarsko() {
     document.getElementById("text1").innerHTML = "2024. 04. 08. 12:00<br>-><br>2024. 04. 09. 19:00<br><br>Будапешт, аэропорт <br>-><br>Wellington<br><br>инструмент: самолет<br>30< часов пути";
     document.getElementById("text2").innerHTML = "Просмотр отеля >";
     document.getElementById("text3").innerHTML = "Первый день";
-    document.getElementById("text6").innerHTML = "Посещение города Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu, места с самым длинным названием в мире.";
+    document.getElementById("text6").innerHTML = "Посещение города <b>Taumatawhakatangi­hangakoauauotamatea­turipukakapikimaunga­horonukupokaiwhen­uakitanatahu</b>, места с самым длинным названием в мире";
     document.getElementById("text5").innerHTML = "Бронирование проживания, экскурсии";
     document.getElementById("text4").innerHTML = "Второй день";
     document.getElementById("text9").innerHTML = "Последний день";
