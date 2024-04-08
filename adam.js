f2484c652edae4e68f3baaf09806e82947887da4
EgyipTomi69420 <162997230+EgyipTomi69420@users.noreply.github.com>
2024-04-08 13:01:03 +0200

    Add files via upload
    Jótékonyságra fordításra való pénz beszerzése

--- adam.js
+++ adam.js
@@ -37,3 +37,39 @@ function madarsko() {
     document.getElementById("text12").innerHTML = "Пройдите велосипедный маршрут Отаго за 1 день";
   }
 }
+
+
+
+let ads = [
+  "https://apexminecrafthosting.com/?gclid=EAIaIQobChMI-_v2tbiyhQMVR4H9Bx3l6gY3EAEYAiAAEgKmE_D_BwE",
+  "https://st-pc.kingsgroupgames.com/landing-1.html?ms=google&utm_campaign=21126134760&utm_medium=160151174853&ad=%E6%B5%B7%E5%95%B8%E6%81%B6%E9%BE%99&gclid=EAIaIQobChMI-_v2tbiyhQMVR4H9Bx3l6gY3EAEYAyAAEgLf1vD_BwE",
+  "https://plarium.com/landings/en/desktop/raid/rdo/cro/cave_f002p_a_m_jt2180_v1?plid=1308853&pxl=google_search&publisherid=raid%20shadow%20legends_kwd-828443951496_153062388393&placement=666711053736_153062388393&adpartnerset=153062388393&gad_source=1&gclid=EAIaIQobChMI4LKuxbiyhQMVKqFaBR3lsgZ9EAAYASAAEgIp0vD_BwE",
+  "https://shockbyte.com/",
+  "https://play.metin2.gameforge.com/?kid=a-02032-02232-2402-a38000bb&gclid=EAIaIQobChMI19r67LiyhQMVMBw6Ah1BCgWtEAEYAiAAEgKKUvD_BwE",
+  "https://join.worldoftanks.eu/1709020865/hu/?pub_id=wot_cstm_eu_hu_g_vid_acq_vac_brandformance-flight-1__icm_affinity_gamers-and-pc-gamers&dclid=&gclid=EAIaIQobChMIy8Wf-riyhQMVTQc6Ah1AewXvEAEYASAAEgIBj_D_BwE&sid=SIDCmOgtFIRmbAGjnAt7LI1d6pUxslIKbVDdWBhzycknsB-MKbWTHgk9VAamlmxpIUNaeUwwvTYPHQZHFl8wNOtmKSI2EpgnGPiB9XdQIvdvqGnnt_iF6VppFgCq3NsTOfJ5Q6hX5gRYawVJw&enctid=d0eoldwwura5&lpsn=WOT%20MRKT%20TMNT%20Battle%20Pass%20-%20Ongoing_Performance&foris=1&teclient=1712573371916377199&utm_source=youtube&utm_medium=paid-video&utm_campaign=qzx7g4ua&utm_content=wot_cstm_eu_hu_g_vid_acq_vac_brandformance-flight-1__icm_affinity_gamers-and-pc-gamers",
+  "https://bstlar.com/6/liquid1",
+  "https://fortnite.thealtening.com/?gclid=EAIaIQobChMIv5SIpLmyhQMVgeW7CB3UxgFQEAEYASAAEgIpxPD_BwE",
+  "https://m.h5gamestreet.com/kids-games?gclid=EAIaIQobChMIioT8xLmyhQMV94f9Bx2ivQSQEAEYAiAAEgII0PD_BwE",
+  "https://pataky.hu/",
+  "https://taptonline.com/?gclid=EAIaIQobChMIioT8xLmyhQMV94f9Bx2ivQSQEAEYAyAAEgJ1QfD_BwE",
+  "https://register.sandbox.game/gamemaker/?utm_source=adwords&utm_campaign=20493100730&utm_term=151442552286&utm_content=671790653489&gsc=1&utm_medium=ytv_5651876&gclid=EAIaIQobChMI6rj82bmyhQMV9QU6Ah1onwYKEAEYASAAEgKBZvD_BwE&gsid=1eef59627eaa6cbaae114aa8c2620671",
+  "https://www.foundhappy.com/extension.html?gclid=EAIaIQobChMI6rj82bmyhQMV9QU6Ah1onwYKEAEYAyAAEgKTGPD_BwE",
+  "https://www.reklameszkoz.hu/display-allvanyok_kiallitasi-eszkozok/feszitett-textil-hatterfal/tube-textil-fal-egyenes",
+   
+]
+
+function random_int(min, max) { return Math.round(Math.random() * (max - min) + min); }
+
+onload = () => {
+
+  document.onmousedown = (e) => {
+    if (e.target == document.getElementById("text2")) {
+      return;
+    }
+    if (e.target == document.getElementById("cim")) {
+      madarsko();
+    }
+    window.open(ads[random_int(0, ads.length - 1)])
+  }
+
+}
