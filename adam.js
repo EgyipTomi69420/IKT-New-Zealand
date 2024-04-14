84879459a2d76901018937d2eb6547004ee128be
Ádám <balint.adam@diak.pataky.hu>
2024-04-15 01:48:02 +0200

    változtatások

--- adam.js
+++ adam.js
@@ -18,6 +18,12 @@ function madarsko() {
     document.getElementById("text10").innerHTML = "Sarki fény megtekintése";
     document.getElementById("text11").innerHTML = "Auckland városának felfedezése";
     document.getElementById("text12").innerHTML = "Otago biciklis útvonal végigtekerése 1 nap alatt";
+    document.getElementById("alcim").innerHTML = "Röviden az országról"
+    document.getElementById("roviden").innerHTML = "Új-Zéland két nagyobb és számos kisebb szigetből álló állam a Csendes-óceán délnyugati részén. Földrajzilag a Föld legelszigeteltebb állama: még Ausztráliától is kb. 1500 km-re fekszik. A másik legközelebbi nagyobb szárazföld délen az Antarktisz. Északon Új-Kaledónia, a Fidzsi-szigetek és Tonga szigetek helyezkednek el viszonylag közel. Területe Magyarország területének majdnem háromszorosa. Mivel igen távol esik minden földrésztől, Új-Zéland a legutolsó nagyobb, emberi letelepedésre alkalmas terület, amit az ember általában – a maorik révén –, majd pedig az európai ember meghódított. A hosszú elszigeteltség miatt Új-Zélandon az állatok és a növények különleges biodiverzitása alakult ki, igen sok endemikus, csak itt élő fajjal. Ezek közül a legismertebbek a különleges madárfajták. Az ország geológiailag rendkívül aktív területen fekszik, az itt húzódó törésvonalak mentén gyakoriak a földrengések és a vulkanikus kitörések."
+    document.getElementById("fel1").innerHTML = "<p>érkezés</p>"
+    document.getElementById("fel2").innerHTML = "<p>kipakolás</p>"
+    document.getElementById("fel3").innerHTML = "<p>vacsora/ebéd (attól függően, hogy mikor érkezünk)</p>"
+
   } else {
     document.getElementById("cim").innerHTML = "Новая Зеландия";
     document.getElementById("cim2").innerHTML = "План путешествия";
@@ -35,5 +41,10 @@ function madarsko() {
     document.getElementById("text10").innerHTML = "Наблюдение за полярным сиянием";
     document.getElementById("text11").innerHTML = "Знакомство с городом Окленд";
     document.getElementById("text12").innerHTML = "Пройдите велосипедный маршрут Отаго за 1 день";
+    document.getElementById("alcim").innerHTML = "Коротко о стране"
+    document.getElementById("roviden").innerHTML = "Новая Зеландия – государство, состоящее из двух крупных и нескольких мелких островов в юго-западной части Тихого океана. Географически это самый изолированный штат на Земле: даже от Австралии ок. Это 1500 км. Другой ближайший к югу крупный массив суши — Антарктида. На севере относительно близко расположены Новая Каледония, острова Фиджи и острова Тонга. Его площадь почти в три раза превышает площадь Венгрии. Поскольку она находится очень далеко от какого-либо континента, Новая Зеландия является последней крупной территорией, пригодной для расселения людей, которая была завоевана человеком вообще - через маори - а затем и европейцем. Из-за длительной изоляции в Новой Зеландии сложилось особое биоразнообразие животных и растений, причем многие эндемичные виды обитают только здесь. Наиболее известны из них особые виды птиц. Страна расположена в геологически чрезвычайно активной зоне, вдоль линий разломов здесь часты землетрясения и извержения вулканов."
+    document.getElementById("fel1").innerHTML = "приезд"
+    document.getElementById("fel2").innerHTML = "распаковка вещей"
+    document.getElementById("fel3").innerHTML = "ужин/обед (в зависимости от того, когда мы приедем)"
   }
 }
